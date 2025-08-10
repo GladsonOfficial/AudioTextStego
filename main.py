5b120bb4ec29f56ace58e8c321c53d3f826f8c52
GladsonT <gladson.thomas.official@gmail.com>
2025-08-11 03:10:04 +0530

    working with some bugs

--- main.py
+++ main.py
@@ -3,6 +3,51 @@ from scipy.io import wavfile
 from scipy import signal
 import char_patterns
 
+CHARACTER_WIDTH_IN_SECONDS = 1
+
+
+def creating_pattern_from_sentence(sentence):
+    sentence_arr = []
+    for i in sentence:
+        sentence_arr.extend(creating_pattern_from_char(i))
+    return sentence_arr
+
+def creating_pattern_from_char(character):
+    pattern = char_patterns.char_pattern[character]
+    arrarr = []
+    arr = []
+    for i in range(len(pattern[0])):
+        for j in pattern:
+            arr.append(j[i])
+        arrarr.append(arr)
+        arr = []
+    
+    return arrarr 
+        
+def apply_bandstop_with_array(pattern_arr, audio_data, sample_rate):
+    start_time = 0
+    pattern_width_time = 1
+    current_time = 0
+
+    start_sample = start_time * sample_rate
+    pattern_width_sample = pattern_width_time * sample_rate
+    # current_sample = current_time * sample_rate
+
+    output_samples_arr = []
+
+    for i in pattern_arr:
+        current_start_time = current_time
+        current_end_time = current_time + pattern_width_time
+        trimmed_section = trim_audio(audio_data, sample_rate, current_start_time, current_end_time)
+        filtered_trimmed_section = apply_bandstop_in_pattern(i, audio_data, sample_rate)
+        output_samples_arr.append(filtered_trimmed_section)
+    
+    output_samples = np.concatenate(output_samples_arr)
+    
+    return output_samples
+
+        
+    
 def apply_bandstop_in_pattern(pattern, audio_data, sample_rate):
     for i, v in enumerate(pattern):
         if v == 1:
@@ -10,13 +55,11 @@ def apply_bandstop_in_pattern(pattern, audio_data, sample_rate):
     
     return audio_data
 
-
 def trim_audio(audio_file, sample_rate, start_time, end_time):
     start_sample = start_time * sample_rate
     end_sample = end_time * sample_rate
 
     return audio_file[start_sample: end_sample]
-
 
 def apply_bandstop_filter(audio_data, sample_rate, low_freq, high_freq, order=8):
     try:
@@ -57,24 +100,26 @@ def apply_bandstop_filter(audio_data, sample_rate, low_freq, high_freq, order=8)
 
 
 if __name__ == "__main__":
+    # creating_pattern_from_char('A')
+
     input_filename = "temp_resources/Mera Bharat.wav" 
     output_filename = "temp_resources/output.wav"
 
-    # low_cutoff_freq = 15000.0
-    # high_cutoff_freq = 16000.0
-    
     sample_rate, wave_file_data = wavfile.read(input_filename)
 
     if wave_file_data.ndim > 1:
         wave_file_data = wave_file_data.mean(axis=1).astype(wave_file_data.dtype)
         print("Convert to mono")
     
-    print(sample_rate, wave_file_data)
-    part1 = trim_audio(wave_file_data, sample_rate, 0, 5)
-    part2 = trim_audio(wave_file_data, sample_rate, 5, 10)
-    part3 = trim_audio(wave_file_data, sample_rate, 10, len(wave_file_data))
+    # print(sample_rate, wave_file_data)
+    # part1 = trim_audio(wave_file_data, sample_rate, 0, 5)
+    # part2 = trim_audio(wave_file_data, sample_rate, 5, 10)
+    # part3 = trim_audio(wave_file_data, sample_rate, 10, len(wave_file_data))
 
-    # part2 = apply_bandstop_filter(part2, sample_rate, low_cutoff_freq, high_cutoff_freq)
-    part2 = apply_bandstop_in_pattern([1,0,0,1,0,1], part2, sample_rate)
-    full_audio = np.concatenate((part1, part2, part3))
+    arr = creating_pattern_from_sentence('AA')
+    full_audio = apply_bandstop_with_array(arr, wave_file_data, sample_rate)
+
+    # # part2 = apply_bandstop_filter(part2, sample_rate, low_cutoff_freq, high_cutoff_freq)
+    # part2 = apply_bandstop_in_pattern([1,0,0,1,0,1], part2, sample_rate)
+    # full_audio = np.concatenate((part1, part2, part3))
     wavfile.write(output_filename, sample_rate, full_audio)
